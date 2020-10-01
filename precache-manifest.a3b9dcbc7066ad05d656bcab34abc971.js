a0832d5596ce19becc723cdb0740b85fe23f8023
Jnanashish <jhandique1999@gmail.com>
2020-10-01 22:09:33 +0530

    Updates

---
+++ precache-manifest.a3b9dcbc7066ad05d656bcab34abc971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cdb766e40a0d4cd1585e904ddd3ce8",
+    "url": "/linktree/index.html"
+  },
+  {
+    "revision": "85f5c0e2b1409d313ffb",
+    "url": "/linktree/static/css/main.f53bba7a.chunk.css"
+  },
+  {
+    "revision": "402c6672e3523025f8d3",
+    "url": "/linktree/static/js/2.f5991f7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/linktree/static/js/2.f5991f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f5c0e2b1409d313ffb",
+    "url": "/linktree/static/js/main.37f1aa9c.chunk.js"
+  },
+  {
+    "revision": "4ec0eb7221d2f3f81314",
+    "url": "/linktree/static/js/runtime-main.e22fb8f2.js"
+  },
+  {
+    "revision": "2c0d067c3afbd0230257fcb491208c0d",
+    "url": "/linktree/static/media/logo.2c0d067c.png"
+  }
+]);
